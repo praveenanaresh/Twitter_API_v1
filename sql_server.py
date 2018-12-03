e0355e87c79136c5939b7f13a6387af60ae38970
praveenanaresh <44649800+praveenanaresh@users.noreply.github.com>
2018-12-03 00:12:44 -0700

    Initial commit

---
+++ sql_server.py
@@ -0,0 +1,38 @@
+
+
+import pyodbc
+
+
+connection_string = 'Driver={ODBC Driver 17 for SQL Server};Server=stairway.usu.edu;Database=Praveena5400;Uid=Praveena5400;Pwd=Praveena@naresh10;'
+
+# Use .connect (DB API 2.0) to get a Connection Object
+conn = pyodbc.connect(connection_string,autocommit=True) # autocommit = True, since it is the SQL Server way
+
+# Create the cursor object
+curs = conn.cursor()
+
+
+
+# Insert some cpi data (R.B.A.R) !
+# We are using the .executemany DB API V2.0 Method
+import csv
+insert_query = 'insert into twitter_analytics_2018 (USER_HASHTAGS,USER_TWEETS,USER_NAME,USER_LOCATION) values (?,?,?,?)'
+with open(r'/Users/pb/Desktop/Documents/MIS_5400/Project/Twitter-API/saved_tweets_new.csv', 'r',encoding='utf8') as cpi_file:
+    cpi = csv.reader(cpi_file)
+    curs.executemany(insert_query, cpi)
+
+
+
+# Commit and close the connection
+conn.commit()
+conn.close()
+
+
+
+
+curs.execute(bulk_insert_sql)
+conn.commit()
+curs.close()
+conn.close()
+
+
